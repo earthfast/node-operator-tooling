5764a5fe0994e540d86d3bb061c81c7642fc4cba
Cheran <cheran.v.senthil@gmail.com>
2025-01-16 20:30:13 +0530

    Cleanup

--- content-node/content-node-register.py
+++ content-node/content-node-register.py
@@ -19,18 +19,25 @@ def run_command(command, parse_json=False):
         return parse_json_str(output.stdout)
     return output
 
+def get_cli_network(input_network):
+    network_map = {
+        "staging": "testnet-sepolia-staging",
+        "testnet": "testnet-sepolia",
+        "testnet-sepolia": "testnet-sepolia"
+    }
+    return network_map.get(input_network)
+
 def main():
     network = input("Enter Network: ")
-
     if network not in ["staging", "testnet", "testnet-sepolia"]:
         print("Network is invalid. Network must be either staging, testnet, or testnet-sepolia")
         exit(1)
-
+    
+    cli_network = get_cli_network(network)
     private_key = input("Enter Private Key: ")
     hostname = input("Enter hostname: ")
     operator_id = input("Enter Operator ID: ")
     region = input("Enter region: ")
-
     # if region is not "us", "eu", "emea", or "apac", exit
     if region not in ["us", "eu", "emea", "apac"]:
         print("region is invalid. region must be either us, eu, emea, or apac")
@@ -38,8 +45,8 @@ def main():
 
     # stake 100 tokens for the node
     STAKE_AMOUNT = 100
-    run_command(f"npx earthfast-cli operator stake {operator_id} {STAKE_AMOUNT} --key {private_key} --network {network}")
-
+    run_command(f"npx earthfast-cli operator stake {operator_id} {STAKE_AMOUNT} --key {private_key} --network {cli_network}")
+    
     # make sure that host is available
     response = requests.get(f"{hostname}/statusz")
     if response.status_code != 200:
@@ -48,19 +55,17 @@ def main():
 
     ENABLED = False
     PRICE = 1.0
-    run_command(f"npx earthfast-cli node create {operator_id} {hostname}:{region}:{ENABLED}:{PRICE} --key {private_key} --network {network}")
-
+    run_command(f"npx earthfast-cli node create {operator_id} {hostname}:{region}:{ENABLED}:{PRICE} --key {private_key} --network {cli_network}")
+    
     print("Node created successfully. Please save the 'nodeId' value from the output above. You can always retrieve this value by running 'npx earthfast-cli node list --network $NETWORK'")
-
-    nodes=run_command(f"npx earthfast-cli node list --network {network} --json", True)
-
+    nodes=run_command(f"npx earthfast-cli node list --network {cli_network} --json", True)
     nodeId = None
     for node in nodes:
         if node.get("hostname") == hostname:
             nodeId = node.get("id")
             break
     
-    run_command(f"npx earthfast-cli node enable {nodeId} true --key {private_key} --network {network}")
+    run_command(f"npx earthfast-cli node enable {nodeId} true --key {private_key} --network {cli_network}")
 
 
 if __name__ == "__main__":
